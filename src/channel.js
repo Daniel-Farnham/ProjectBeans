fdd2ece221335e4ec9de06de1bbba977cd66f0f6
Curtis Scully <z5361935@ad.unsw.edu.au>
2022-09-18 21:12:44 +1000

    Stub function for channelDetailsV1

--- src/channel.js
+++ src/channel.js
@@ -1,3 +1,27 @@
+function channelDetailsV1(authUserId, channelId) {
+    return {
+        name: 'Hayden',
+        ownerMembers: [
+          {
+            uId: 1,
+            email: 'example@gmail.com',
+            nameFirst: 'Hayden',
+            nameLast: 'Jacobs',
+            handleStr: 'haydenjacobs',
+          }
+        ],
+        allMembers: [
+          {
+            uId: 1,
+            email: 'example@gmail.com',
+            nameFirst: 'Hayden',
+            nameLast: 'Jacobs',
+            handleStr: 'haydenjacobs',
+          }
+        ],
+    };
+}
+
 function channelJoinV1(authUserId, channelId) {
     return {};
 }
