3ab924d6c4e7b84c928c5dfa113d40bb0f25e3c3
Hang Pham <z5425016@ad.unsw.edu.au>
2022-10-01 21:18:59 +1000

    Implementation for channelsListAllV1

--- src/channels.js
+++ src/channels.js
@@ -1,3 +1,5 @@
+import { getData, setData } from './dataStore.js';
+
 // channelsListV1 function with stub response
 function channelsListV1(authUserId) {
     return {
@@ -12,14 +14,15 @@ function channelsListV1(authUserId) {
 
 // channelsListAllV1 function with stub response
 function channelsListAllV1(authUserId) {
-    return {
-        channels: [
-          {
-            channelId: 1,
-            name: 'My Channel',
-          }
-        ],
-    };
+  // Check authUserId exists
+  // If exists, return channels
+  const data = getData();
+
+  let channels = [];
+  for (let channel of data.channels) {
+    channels.push(channel);
+  }
+  return channels;
 }
 
 // channelsCreateV1 function with stub response
