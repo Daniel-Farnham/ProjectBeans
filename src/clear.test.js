80ad9d5ba67838637d98a982202ff9abc207d778
Hang Pham <z5425016@ad.unsw.edu.au>
2022-10-01 19:10:54 +1000

    Test case when attempting to create the same user after running clearV1

--- src/clear.test.js
+++ src/clear.test.js
@@ -3,13 +3,20 @@ import { authLoginV1, authRegisterv1 } from './auth';
 
 
 test('Testing clearV1 returns {}', () => {
-  let result = clearV1();
+  const result = clearV1();
   expect(result).toMatchObject({});
 });
 
 test('Testing logging in as a user should fail after running clearV1', () => {
   authRegisterV1('hang.pham1@student.unsw.edu.au', 'AP@ssW0rd!', 'Hang', 'Pham');
   clearV1();
-  let result = authLoginV1('hang.pham1@student.unsw.edu.au', 'AP@ssW0rd!');
+  const result = authLoginV1('hang.pham1@student.unsw.edu.au', 'AP@ssW0rd!');
   expect(result).toMatchObject({error: 'error'});
+});
+
+test('Testing re-creating the same user after running clearV1', () => {
+  authRegisterV1('hang.pham1@student.unsw.edu.au', 'AP@ssW0rd!', 'Hang', 'Pham');
+  clearV1();
+  const result = authRegisterV1('hang.pham1@student.unsw.edu.au', 'AP@ssW0rd!', 'Hang', 'Pham');
+  expect(typeof result).toBe('number');
 });
