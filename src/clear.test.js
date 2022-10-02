798b13763fd936d8567b16699fff40582e0fe860
Hang Pham <z5425016@ad.unsw.edu.au>
2022-10-02 20:35:30 +1100

    Move tests into describe block

--- src/clear.test.js
+++ src/clear.test.js
@@ -1,22 +1,24 @@
 import { clearV1 } from './other';
 import { authLoginV1, authRegisterv1 } from './auth';
 
+describe('ClearV1 test cases', () => {
+  test('Testing clearV1 returns {}', () => {
+    const result = clearV1();
+    expect(result).toMatchObject({});
+  });
+  
+  test('Testing logging in as a user should fail after running clearV1', () => {
+    authRegisterV1('hang.pham1@student.unsw.edu.au', 'AP@ssW0rd!', 'Hang', 'Pham');
+    clearV1();
+    const result = authLoginV1('hang.pham1@student.unsw.edu.au', 'AP@ssW0rd!');
+    expect(result).toMatchObject({error: 'error'});
+  });
 
-test('Testing clearV1 returns {}', () => {
-  const result = clearV1();
-  expect(result).toMatchObject({});
+  test('Testing re-creating the same user after running clearV1', () => {
+    authRegisterV1('hang.pham1@student.unsw.edu.au', 'AP@ssW0rd!', 'Hang', 'Pham');
+    clearV1();
+    const result = authRegisterV1('hang.pham1@student.unsw.edu.au', 'AP@ssW0rd!', 'Hang', 'Pham');
+    expect(typeof result).toBe('number');
+  });
 });
 
-test('Testing logging in as a user should fail after running clearV1', () => {
-  authRegisterV1('hang.pham1@student.unsw.edu.au', 'AP@ssW0rd!', 'Hang', 'Pham');
-  clearV1();
-  const result = authLoginV1('hang.pham1@student.unsw.edu.au', 'AP@ssW0rd!');
-  expect(result).toMatchObject({error: 'error'});
-});
-
-test('Testing re-creating the same user after running clearV1', () => {
-  authRegisterV1('hang.pham1@student.unsw.edu.au', 'AP@ssW0rd!', 'Hang', 'Pham');
-  clearV1();
-  const result = authRegisterV1('hang.pham1@student.unsw.edu.au', 'AP@ssW0rd!', 'Hang', 'Pham');
-  expect(typeof result).toBe('number');
-});
