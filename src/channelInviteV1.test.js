d75648fcfdfc67b3737844df7dd814c71bf8d32a
Hang Pham <z5425016@ad.unsw.edu.au>
2022-10-04 22:31:27 +1100

    Initial test file set up

---
+++ src/channelInviteV1.test.js
@@ -0,0 +1,10 @@
+import { clearV1 } from './other';
+import { channelsCreateV1} from './channels';
+import { authRegisterV1 } from './auth';
+
+describe('Working cases', () => {
+
+});
+
+describe('Fail cases', () => {
+});
