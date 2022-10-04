1180029c12accd9fbcdf6e3ce4fece2f94223db0
Hang Pham <z5425016@ad.unsw.edu.au>
2022-10-04 22:20:16 +1100

    Implementation code for userProfileV1

--- src/users.js
+++ src/users.js
@@ -1,6 +1,26 @@
-// Get user profile 
+import { getData } from './dataStore.js';
+import { userIdExists } from './other.js';
+
+// userProfileV1 function
+// Parameters: authUserId, uId
+// Return: Returns valid user ID, email, first name, last name, and handle
 function userProfileV1 (authUserId, uId) {
-  return 1;
+  // Check if authUserId exists
+  if (userIdExists(authUserId)) {
+    // Check if userId to search exists, then return user data
+    if (userIdExists(authUserId)) {
+      const data = getData();
+      for (const user of data.users) {
+        if (user.uId === uId) {
+          return user;
+        }
+      }
+    } else {
+      return { error: "userId to search is invalid" };
+    }
+  } else {
+    return { error: "authUserId is invalid" };
+  }
 };
 
 export { userProfileV1 };
