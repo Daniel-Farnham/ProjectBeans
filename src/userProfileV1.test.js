bc665b4877f45ff21c6e1779e944f247c6291a4b
Hang Pham <z5425016@ad.unsw.edu.au>
2022-10-04 20:13:49 +1100

    Update test error cases to match any string type

--- src/userProfileV1.test.js
+++ src/userProfileV1.test.js
@@ -2,6 +2,11 @@ import { clearV1 } from './other';
 import { userProfileV1 } from './users';
 import { authLoginV1, authRegisterv1 } from './auth';
 
+// Before each test, clear dataStore
+beforeEach(() => {
+  clearV1();
+});
+
 // Working cases
 test('Testing successful return of user profile', () => {
   const userId = authRegisterV1('hang.pham1@student.unsw.edu.au', 'AP@ssW0rd!', 'Hang', 'Pham');
@@ -19,21 +24,18 @@ test('Testing successful return of user profile', () => {
   expect(resultUser).toMatchObject(expectedUser);
 });
 
-describe('Fail cases', () => {
-  // Clear data store and create new user
-  beforeEach(() => {
-    clearV1();
-    const authUserId = authRegisterV1('jane.doe@student.unsw.edu.au', 'AP@ssW0rd!', 'Jane', 'Doe');
-  });
   
-  test('uID to search does not exist', () => {
-    const result = userProfileV1(authUserId, -1000);
-    expect(result).toMatchObject({error: 'error'});
-  });
-  
-  test('authUserId does not exist', () => {
-    const result = userProfileV1(-1000, authUserId);
-    expect(result).toMatchObject({error: 'error'});
-  });
-
+describe('Testing userProfileV1 error handling', () => {
+    test.each([
+      { aUId: 0, uId: 100, desc: 'uID to search does not exist' },
+      { aUId: 100, uId: 0, desc: 'authUserId does not exist' },
+    ])('$desc', ({ aUId, uId }) => {
+      const authUserId = authRegisterV1('jane.doe@student.unsw.edu.au', 'AP@ssW0rd!', 'Jane', 'Doe');
+      const result = userProfileV1(authUserId + aUId, authUserId + uId);
+      expect(result).toStrictEqual(
+        {
+          error: expect.any(String),
+        }
+      );
+    });
 });
