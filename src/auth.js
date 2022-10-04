4e058b1b12dfd013d23b15ce25d2c4be40beef91
Curtis Scully <z5361935@ad.unsw.edu.au>
2022-10-04 23:40:11 +1100

    Begun implementation of authLoginV1.

--- src/auth.js
+++ src/auth.js
@@ -1,8 +1,15 @@
 // authLoginV1 function with stub response
 function authLoginV1(authUserId, email, password) {
-    return {
-        authUserId: 1,
-    };
+  // If a user exists with matching email and password, return authUserId
+  const data = getData();
+  for (const user of data.users) {
+    if (user.email === email && user.password === password) {
+      return { authUserId: user.uId };
+    }
+  }
+
+  // If haven't returned yet, email isn't registered or password is wrong
+  return { error: 'error' };
 }
 
 // authRegisterV1 function with stub response
