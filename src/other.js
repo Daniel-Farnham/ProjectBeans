91a6ebf9874a6a4c04fc15a58a5f6f89f6084f6c
Curtis Scully <z5361935@ad.unsw.edu.au>
2022-10-05 21:20:12 +1100

    Fixed bugs and styling for authRegisterV1, moved helper functions into more appropriate location and updated default dataStore structure.

--- src/other.js
+++ src/other.js
@@ -1,5 +1,4 @@
 import { getData, setData } from './dataStore.js';
-import { validator } from 'validator';
 
 // Function to clear the data store object
 function clearV1 () {
@@ -37,36 +36,4 @@ function channelIdExists(channelId) {
   return false;
 }
 
-// Check if the information used to register a new account is valid
-function registerInfoInvalid(email, password, nameFirst, nameLast, data) {
-  let isInvalid = false;
-  // Check whether email, password and first/last name meet the criteria
-  if (!(validator(email)) || password.length < 6 || 
-      nameFirst.length < 1 || nameFirst.length > 50 ||
-      nameLast.length < 1 || nameLast.length > 50) {
-    
-    isInvalid = true;
-  }
-  
-  // Check if the email is in use
-  for (const user of data.users) {
-    if (email === user.email) {
-      isInvalid = true;
-    }
-  }
-  
-  return isInvalid;
-}
-
-// Check if a handle string exists in database
-function handleExists(handleStr, data) {
-  // Loop through users array to check if the handle already exists
-  for (const user in data.users) {
-    if (user.handleStr === handleStr) {
-      return true;
-    }
-  }
-  return false;
-}
-
-export { userIdExists, channelIdExists, registerInfoInvalid, handleExists, clearV1 };
+export { userIdExists, channelIdExists, clearV1 };
