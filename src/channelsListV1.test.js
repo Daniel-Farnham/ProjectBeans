5a9638fd8270f7393948e673d384384ba89b487b
z5361315@ad.unsw.edu.au <z5361315@ad.unsw.edu.au>
2022-10-06 21:32:16 +1100

    Fixed indentation for channelsListV1.test.js

--- src/channelsListV1.test.js
+++ src/channelsListV1.test.js
@@ -3,36 +3,32 @@ import { channelsCreateV1, channelsListV1 } from './channels';
 import { authRegisterV1 } from './auth';
 
 describe('Testing channelsListV1', () => {
-    test('Test successful return of users channels', () => {
-        const userId = authRegisterV1('edwin.ngo@student.unsw.edu.au', 'ANicePassword', 'Edwin', 'Ngo');
-        const channelId1 = channelsCreateV1(userId.authUserId, "General" , true);
-        const channelId2 = channelsCreateV1(userId.authUserId, "Boost", false);
-        const channelId3 = channelsCreateV1(userId.authUserId + 1, "Aero", false);
-        const expectedChannels = [
-            {
-                channelId: channelId1,
-                name: "General",
-            },
-            {
-                channelId: channelId2,
-                name: "Boost",
-            },
-        ];
+  test('Test successful return of users channels', () => {
+    const userId = authRegisterV1('edwin.ngo@student.unsw.edu.au', 'ANicePassword', 'Edwin', 'Ngo');
+    const channelId1 = channelsCreateV1(userId.authUserId, "General" , true);
+    const channelId2 = channelsCreateV1(userId.authUserId, "Boost", false);
+    const channelId3 = channelsCreateV1(userId.authUserId + 1, "Aero", false);
+    const expectedChannels = [
+      {
+        channelId: channelId1,
+        name: "General",
+      },
+      {
+        channelId: channelId2,
+        name: "Boost",
+      },
+    ];
 
-        const resultChannels = channelsListV1(userId.authUserId);
-        
-        expect(resultChannels).toMatchObject(expectedChannels);
-    });
+    const resultChannels = channelsListV1(userId.authUserId);
 
-    test('Testing invalid authUserId', () => {
-        const userId = authRegisterV1('edwin.ngo@student.unsw.edu.au', 'ANicePassword', 'Edwin', 'Ngo');
-        const channelId1 = channelsCreateV1(userId.authUserId, "General", true);
-        const resultChannels = channelsListV1(userId.authUserId + 1);
+    expect(resultChannels).toMatchObject(expectedChannels);
+  });
 
-        expect(resultChannels).toStrictEqual(
-            {
-                error: expect.any(String),
-            }
-        );
-    });
+  test('Testing invalid authUserId', () => {
+    const userId = authRegisterV1('edwin.ngo@student.unsw.edu.au', 'ANicePassword', 'Edwin', 'Ngo');
+    const channelId1 = channelsCreateV1(userId.authUserId, "General", true);
+    const resultChannels = channelsListV1(userId.authUserId + 1);
+
+    expect(resultChannels).toStrictEqual({error: expect.any(String)});
+  });
 });
