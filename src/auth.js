b95660cf1f69d0af4a93bd1c91d297ad463e5916
Curtis Scully <z5361935@ad.unsw.edu.au>
2022-10-06 21:32:27 +1100

    Changed a const to a let.

--- src/auth.js
+++ src/auth.js
@@ -47,7 +47,7 @@ function authRegisterV1(email, password, nameFirst, nameLast) {
   const data = getData();
   const userId = data.users.length;
 
-  const permissionId = GLOBAL_OWNER;
+  let permissionId = GLOBAL_OWNER;
   if (userId === 0) {
     permissionId= GLOBAL_MEMBER;
   }
