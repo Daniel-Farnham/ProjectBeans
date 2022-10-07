88b76e3ca3e8991b6cdd7af5fa4754b53684401f
Hang Pham <z5425016@ad.unsw.edu.au>
2022-10-07 19:03:29 +1100

    Clean up userProfileV1 function and fix test for success case

--- src/users.js
+++ src/users.js
@@ -1,36 +1,38 @@
 import { getData } from './dataStore.js';
 import { userIdExists } from './other.js';
 
-// userProfileV1 function
-// Parameters: authUserId, uId
-// Return: Returns valid user ID, email, first name, last name, and handle
-
 /**
   * Returns user object if a valid user is found
   * 
   * @param {number} authUserId - uId of authorised user requesting profile
   * @param {number} uId - uId of user to search
   * 
-  * @returns {user} - Returns valid user ID, email, first name, last name, 
+  * @returns {user} - Returns object with valid user ID, email, first name, last name, 
   * and handle
 */
 function userProfileV1 (authUserId, uId) {
-  // Check if authUserId exists
-  if (userIdExists(authUserId)) {
-    // Check if userId to search exists, then return user data
-    if (userIdExists(uId)) {
-      const data = getData();
-      for (const user of data.users) {
-        if (user.uId === uId) {
-          return { user };
-        }
-      }
-    } else {
-      return { error: "userId to search is invalid" };
-    }
-  } else {
-    return { error: "authUserId is invalid" };
+  // If either uId or authUserId does not exist, then return error
+  if (!userIdExists(authUserId) || !userIdExists(uId)) {
+    return { error: "authUserId/uId to search is invalid" };
   }
+  
+  // Retrieve user profile for matching user
+  const data = getData();
+  for (const user of data.users) {
+    if (user.uId === uId) {
+      return { 
+        user: {
+          uId: user.uId,
+          email: user.email,
+          nameFirst: user.nameFirst,
+          nameLast: user.nameLast,
+          handleStr: user.handleStr,
+        }
+       };
+    }
+  }
+
 };
 
 export { userProfileV1 };
+
