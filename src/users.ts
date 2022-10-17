0a2c894897f17dc600c23f7a45fd1619d92e9a81
z5361315@ad.unsw.edu.au <z5361315@ad.unsw.edu.au>
2022-10-17 20:20:12 +1100

    Added return types

--- src/users.ts
+++ src/users.ts
@@ -1,26 +1,26 @@
-import { getData } from './dataStore.js';
-import { userIdExists } from './other.js';
+import { getData } from './dataStore';
+import { userIdExists, User, error } from './other';
 
 /**
   * Returns user object if a valid user is found
-  * 
+  *
   * @param {number} authUserId - uId of authorised user requesting profile
   * @param {number} uId - uId of user to search
-  * 
-  * @returns {user} - Returns object with valid user ID, email, first name, last name, 
+  *
+  * @returns {user} - Returns object with valid user ID, email, first name, last name,
   * and handle
 */
-function userProfileV1 (authUserId: number, uId: number) {
+function userProfileV1 (authUserId: number, uId: number): error | { user: User } {
   // If either uId or authUserId does not exist, then return error
   if (!userIdExists(authUserId) || !userIdExists(uId)) {
-    return { error: "authUserId/uId to search is invalid" };
+    return { error: 'authUserId/uId to search is invalid' };
   }
-  
+
   // Retrieve user profile for matching user
   const data = getData();
   for (const user of data.users) {
     if (user.uId === uId) {
-      return { 
+      return {
         user: {
           uId: user.uId,
           email: user.email,
@@ -28,11 +28,9 @@ function userProfileV1 (authUserId: number, uId: number) {
           nameLast: user.nameLast,
           handleStr: user.handleStr,
         }
-       };
+      };
     }
   }
-
-};
+}
 
 export { userProfileV1 };
-
