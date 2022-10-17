a1793a30fc355c64ce83c41dff39a8c8cf06220f
z5361315@ad.unsw.edu.au <z5361315@ad.unsw.edu.au>
2022-10-17 18:40:52 +1100

    Converted dataStore.js to typescript

---
+++ src/dataStore.ts
@@ -0,0 +1,42 @@
+import { Channel, User } from './other';
+
+export interface DataStore {
+  users: Array<User>;
+  channels: Array<Channel>;
+};
+
+// YOU SHOULD MODIFY THIS OBJECT BELOW
+let data: DataStore = {
+
+  users: [],
+  channels: [],
+
+};
+
+// YOU SHOULDNT NEED TO MODIFY THE FUNCTIONS BELOW IN ITERATION 1
+
+/*
+Example usage
+    let store = getData()
+    console.log(store) # Prints { 'names': ['Hayden', 'Tam', 'Rani', 'Giuliana', 'Rando'] }
+
+    names = store.names
+
+    names.pop()
+    names.push('Jake')
+
+    console.log(store) # Prints { 'names': ['Hayden', 'Tam', 'Rani', 'Giuliana', 'Jake'] }
+    setData(store)
+*/
+
+// Use get() to access the data
+function getData() {
+  return data;
+}
+
+// Use set(newData) to pass in the entire data object, with modifications made
+function setData(newData: DataStore) {
+  data = newData;
+}
+
+export { getData, setData };
