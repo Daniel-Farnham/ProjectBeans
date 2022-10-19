9770da3da36939c3d44ab18461602330db94387a
Hang Pham <z5425016@unsw.edu.au>
2022-10-19 13:33:50 +1100

    Convert clearV1 tests to HTTP

--- src/clear.test.ts
+++ src/clear.test.ts
@@ -1,16 +1,27 @@
-import { clearV1 } from './other.js';
-import { authLoginV1, authRegisterV1 } from './auth.js';
+import { postRequest, deleteRequest } from './other';
+
+import { port, url } from './config.json';
+const SERVER_URL = `${url}:${port}`;
 
 describe('ClearV1 test cases', () => {
   test('Testing clearV1 returns {}', () => {
-    const result = clearV1();
+    const result = deleteRequest(SERVER_URL + '/clear/v1', {});
     expect(result).toMatchObject({});
   });
 
   test('Testing logging in as a user should fail after running clearV1', () => {
-    authRegisterV1('hang.pham1@student.unsw.edu.au', 'AP@ssW0rd!', 'Hang', 'Pham');
-    clearV1();
-    const result = authLoginV1('hang.pham1@student.unsw.edu.au', 'AP@ssW0rd!');
+    postRequest(SERVER_URL + '/auth/register/v2', {
+      email: 'hang.pham1@student.unsw.edu.au',
+      password: 'AP@ssW0rd!',
+      nameFirst: 'Hang',
+      nameLast: 'Pham',
+    });
+  
+    deleteRequest(SERVER_URL + '/clear/v1', {});
+    const result = postRequest(SERVER_URL + '/auth/login/v2', {
+      email: 'hang.pham1@student.unsw.edu.au',
+      password: 'AP@ssW0rd!',
+    });
     expect(result).toStrictEqual(
       {
         error: expect.any(String),
@@ -19,9 +30,19 @@ describe('ClearV1 test cases', () => {
   });
 
   test('Testing re-creating the same user after running clearV1', () => {
-    authRegisterV1('hang.pham1@student.unsw.edu.au', 'AP@ssW0rd!', 'Hang', 'Pham');
-    clearV1();
-    const result = authRegisterV1('hang.pham1@student.unsw.edu.au', 'AP@ssW0rd!', 'Hang', 'Pham');
+    postRequest(SERVER_URL + '/auth/register/v2', {
+      email: 'hang.pham1@student.unsw.edu.au',
+      password: 'AP@ssW0rd!',
+      nameFirst: 'Hang',
+      nameLast: 'Pham',
+    });
+    deleteRequest(SERVER_URL + '/clear/v1', {});
+    const result = postRequest(SERVER_URL + '/auth/register/v2', {
+      email: 'hang.pham1@student.unsw.edu.au',
+      password: 'AP@ssW0rd!',
+      nameFirst: 'Hang',
+      nameLast: 'Pham',
+    });
     expect(result).toStrictEqual(
       {
         authUserId: expect.any(Number),
