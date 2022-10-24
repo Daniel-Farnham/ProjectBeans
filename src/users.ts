792ecd019bf99f76f2aaae450f21364e695f9717
Hang Pham <hang.pham1@student.unsw.edu.au>
2022-10-25 00:44:09 +1100

    Revert "Merge branch 'revert-80639d2c' into 'master'"
    This reverts merge request !94

--- src/users.ts
+++ src/users.ts
@@ -1,4 +1,4 @@
-import { getData } from './dataStore';
+import { getData, setData } from './dataStore';
 import { userIdExists, tokenExists, User, error } from './other';
 
 /**
@@ -63,6 +63,56 @@ export function usersAllV1 (token: string): error | {users: any[]} {
     );
   }
   return { users };
+}
+
+/**
+  * Returns user object if a valid user is found
+  *
+  * @param {string} token - token session for user requesting profile
+  * @param {string} nameFirst - user's first name to change to
+  * @param {string} nameLast - user's last name to change to
+  *
+  * @returns {{}} - Returns empty object upon successful name change
+*/
+export function userProfileSetNameV1 (token: string, nameFirst: string, nameLast: string): error | Record<string, never> {
+  if (!tokenExists(token)) {
+    return { error: 'token provided is invalid' };
+  }
+
+  if (!validName(nameFirst) || !validName(nameLast)) {
+    return { error: 'length of nameFirst/nameLast is not between 1 and 50' };
+  }
+
+  // Update user profile for matching user with new names
+  const uId = getUidFromToken(token);
+
+  const data = getData();
+  for (const user of data.users) {
+    if (user.uId === uId) {
+      user.nameFirst = nameFirst;
+      user.nameLast = nameLast;
+    }
+  }
+
+  setData(data);
+  return {};
+}
+
+function validName(name: string): boolean {
+  if (name.length >= 1 && name.length <= 50) {
+    return true;
+  }
+  return false;
+}
+
+function getUidFromToken (token: string) {
+  const data = getData();
+
+  for (const session of data.sessions) {
+    if (session.tokens.includes(token)) {
+      return session.uId;
+    }
+  }
 }
 
 export { userProfileV1 };
