a76f9d50d2bb6b3a78f69f9c84e7138ee0080164
Hang Pham <z5425016@unsw.edu.au>
2022-10-25 22:45:28 +1100

    Test cases for auth/logout/v1

---
+++ src/authLogoutV1.test.ts
@@ -0,0 +1,106 @@
+import { getRequest, postRequest, deleteRequest } from './other';
+
+import { port, url } from './config.json';
+const SERVER_URL = `${url}:${port}`;
+
+
+beforeEach(() => {
+  deleteRequest(SERVER_URL + '/clear/v1', {});
+});
+
+describe('Testing successful cases for authLogoutV1', () => {
+
+  test('Testing returning empty object upon successful logout', () => {
+
+    const user = postRequest(SERVER_URL + '/auth/register/v2', {
+      email: 'hang.pham1@student.unsw.edu.au',
+      password: 'password',
+      nameFirst: 'Hang',
+      nameLast: 'Pham'
+    });
+
+    const result = postRequest(SERVER_URL + '/auth/logout/v1', {
+      token: user.token,
+    });
+
+    expect(result).toStrictEqual({});
+  });
+
+  test('Testing unsuccessful user profile access when using token that is no longer valid', () => {
+
+    const user = postRequest(SERVER_URL + '/auth/register/v2', {
+      email: 'hang.pham1@student.unsw.edu.au',
+      password: 'password',
+      nameFirst: 'Hang',
+      nameLast: 'Pham'
+    });
+
+    postRequest(SERVER_URL + '/auth/logout/v1', {
+      token: user.token,
+    });
+
+    const result = getRequest(SERVER_URL + '/user/profile/v2', {
+      token: user.token,
+      uId: user.authUserId,
+    });
+
+    expect(result).toMatchObject({ error: expect.any(String) });
+  });
+
+  test('Testing ability to login with another token if logged out of one token', () => {
+
+    const user = postRequest(SERVER_URL + '/auth/register/v2', {
+      email: 'hang.pham1@student.unsw.edu.au',
+      password: 'password',
+      nameFirst: 'Hang',
+      nameLast: 'Pham'
+    });
+    
+    const loggedInSession = postRequest(SERVER_URL + '/auth/login/v2', {
+      email: 'hang.pham1@student.unsw.edu.au',
+      password: 'password'
+    });
+
+    postRequest(SERVER_URL + '/auth/logout/v1', {
+      token: user.token,
+    });
+
+    const result = getRequest(SERVER_URL + '/user/profile/v2', {
+      token: loggedInSession.token,
+      uId: user.authUserId,
+    });
+
+    const expectedResult = {
+      user: {
+        uId: user.authUserId,
+        email: 'hang.pham1@student.unsw.edu.au',
+        nameFirst: 'Hang',
+        nameLast: 'Pham',
+        handleStr: 'hangpham',
+      }
+    };
+    expect(result).toMatchObject(expectedResult);
+  });
+
+});
+
+describe('Testing authLogoutV1 error handling', () => {
+  test.each([
+    { token: 'InvalidToken', desc: 'Testing invalid token' },
+  ])('$desc', ({ token }) => {
+
+
+    const user = postRequest(SERVER_URL + '/auth/register/v2', {
+      email: 'hang.pham1@student.unsw.edu.au',
+      password: 'password',
+      nameFirst: 'Hang',
+      nameLast: 'Pham'
+    });
+
+    const result = postRequest(SERVER_URL + '/auth/logout/v1', {
+      token: user.token + token,
+    });
+
+    expect(result).toMatchObject({ error: expect.any(String) });
+  });
+});
