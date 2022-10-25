fbffe54cf88b0b10fa1a8e54edb155508bc81a5a
Daniel Farnham <z5115421@ad.unsw.edu.au>
2022-10-25 11:56:40 +1100

    lint fixes

--- src/channelDetailsV1.test.ts
+++ src/channelDetailsV1.test.ts
@@ -1,17 +1,13 @@
-import { getRequest, postRequest, deleteRequest } from './other'; 
-import { port, url } from './config.json'; 
-const SERVER_URL = `${url}:${port}`
-
+import { getRequest, postRequest, deleteRequest } from './other';
+import { port, url } from './config.json';
+const SERVER_URL = `${url}:${port}`;
 
 describe('Testing channelDetails', () => {
   beforeEach(() => {
     deleteRequest(SERVER_URL + '/clear/v1', {});
   });
 
-
-  
   test('Successfully view channel details', () => {
-
     // post request sending data to authRegisterV1
     // const userId = authRegisterV1('daniel.farnham@student.unsw.edu.au', 'AVeryPoorPassword', 'Daniel', 'Farnham');
     const userId = postRequest(SERVER_URL + '/auth/register/v2', {
@@ -21,17 +17,17 @@ describe('Testing channelDetails', () => {
       nameLast: 'Farnham',
     });
 
-    //post request sending data to channelsCreateV1 
-    //const channel = channelsCreateV1(userId.authUserId, 'ChannelBoost', true);
+    // post request sending data to channelsCreateV1
+    // const channel = channelsCreateV1(userId.authUserId, 'ChannelBoost', true);
 
     const channel = postRequest(SERVER_URL + '/channels/create/v2', {
-      token: userId.token, 
+      token: userId.token,
       name: 'ChannelBoost',
-      IsPublic: true, 
+      IsPublic: true,
     });
-    
-    // get request to receve data back from channelDetailsV1 
-    //const ReturnedChannelObj = channelDetailsV1(userId.token, channel.channelId); // Should provide a valid channelId
+
+    // get request to receve data back from channelDetailsV1
+    // const ReturnedChannelObj = channelDetailsV1(userId.token, channel.channelId); // Should provide a valid channelId
 
     const ReturnedChannelObj = getRequest(SERVER_URL + '/channel/details/v2', {
       token: userId.token,
@@ -61,12 +57,11 @@ describe('Testing channelDetails', () => {
         }
       ],
     };
-    // checking that we have a match 
+    // checking that we have a match
     expect(ReturnedChannelObj).toMatchObject(ExpectedChannelObj);
   });
 
   test('Testing invalid authUserId', () => {
-    
     // post request sending data to authRegisterV1
     // const userId = authRegisterV1('daniel.farnham@student.unsw.edu.au', 'AVeryPoorPassword', 'Daniel', 'Farnham');
     const userId = postRequest(SERVER_URL + '/auth/register/v2', {
@@ -76,15 +71,14 @@ describe('Testing channelDetails', () => {
       nameLast: 'Farnham',
     });
 
-    // post request sending data to channelsCreateV1 
+    // post request sending data to channelsCreateV1
     // const channel = channelsCreateV1(userId.authUserId, 'ChannelBoost', true);
     const channel = postRequest(SERVER_URL + '/channels/create/v2', {
-      token: userId.token, 
+      token: userId.token,
       name: 'ChannelBoost',
-      IsPublic: true, 
-    })
+      IsPublic: true,
+    });
 
-    
     // const ReturnedChannelObject = channelDetailsV1(userId.token + 1, channel.channelId);
     const ReturnedChannelObj = getRequest(SERVER_URL + '/channel/details/v2', {
       token: userId.token,
@@ -95,7 +89,6 @@ describe('Testing channelDetails', () => {
   });
 
   test('Testing invalid channelId', () => {
-
     // post request sending data to authRegisterV1
     // const userId = authRegisterV1('daniel.farnham@student.unsw.edu.au', 'AVeryPoorPassword', 'Daniel', 'Farnham');
     const userId = postRequest(SERVER_URL + '/auth/register/v2', {
@@ -105,14 +98,14 @@ describe('Testing channelDetails', () => {
       nameLast: 'Farnham',
     });
 
-    // post request sending data to channelsCreateV1 
+    // post request sending data to channelsCreateV1
     // const channel = channelsCreateV1(userId.authUserId, 'ChannelBoost', true);
     const channel = postRequest(SERVER_URL + '/channels/create/v2', {
-      token: userId.token, 
+      token: userId.token,
       name: 'ChannelBoost',
-      IsPublic: true, 
-    })
-    
+      IsPublic: true,
+    });
+
     // const ReturnedChannelObject = channelDetailsV1(userId.token, channel.channelId + 1);
     const ReturnedChannelObj = getRequest(SERVER_URL + '/channel/details/v2', {
       token: userId.token,
@@ -122,7 +115,6 @@ describe('Testing channelDetails', () => {
   });
 
   test('Authorised user is not a member of the channel', () => {
-
     // post request sending data to authRegisterV1
     // const userId = authRegisterV1('daniel.farnham@student.unsw.edu.au', 'AVeryPoorPassword', 'Daniel', 'Farnham');
     const userId = postRequest(SERVER_URL + '/auth/register/v2', {
@@ -132,25 +124,25 @@ describe('Testing channelDetails', () => {
       nameLast: 'Farnham',
     });
 
-    // post request sending data to channelsCreateV1 
+    // post request sending data to channelsCreateV1
     // const channel = channelsCreateV1(userId.authUserId, 'ChannelBoost', true);
     const channel = postRequest(SERVER_URL + '/channels/create/v2', {
-      token: userId.token, 
+      token: userId.token,
       name: 'ChannelBoost',
-      IsPublic: true, 
+      IsPublic: true,
     });
 
     // const NonMemberChannel = channelsCreateV1(userId.authUserId, 'ChannelBoostWithoutDaniel', true);
     const NonMemberChannel = postRequest(SERVER_URL + '/channels/create/v2', {
-      token: userId.token, 
+      token: userId.token,
       name: 'ChannelBoostWithoutDaniel',
-      IsPublic: true, 
+      IsPublic: true,
     });
-    
-    //expect(NonMemberChannel).toMatchObject({ error: expect.any(String) });
-    //const userId = authRegisterV1('daniel.farnham@student.unsw.edu.au', 'AVeryPoorPassword', 'Daniel', 'Farnham');
-    //channelsCreateV1(userId.authUserId, 'ChannelBoost', true);
-    
+
+    // expect(NonMemberChannel).toMatchObject({ error: expect.any(String) });
+    // const userId = authRegisterV1('daniel.farnham@student.unsw.edu.au', 'AVeryPoorPassword', 'Daniel', 'Farnham');
+    // channelsCreateV1(userId.authUserId, 'ChannelBoost', true);
+
     expect(NonMemberChannel).toMatchObject({ error: expect.any(String) });
   });
 });
