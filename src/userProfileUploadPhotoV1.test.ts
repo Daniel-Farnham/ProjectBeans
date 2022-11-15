bc677fe48140237210498f500bc143774e47b67b
Hang Pham <z5425016@unsw.edu.au>
2022-11-15 21:08:40 +1100

    Test cases for user/profile/uploadphoto/v1

--- src/userProfileUploadPhotoV1.test.ts
+++ src/userProfileUploadPhotoV1.test.ts
@@ -1,20 +1,69 @@
 import { FORBIDDEN, BAD_REQUEST } from './other';
 import {
-  clearV1, authRegisterV1, notificationsGetV1, dmCreateV1, channelMessagesV1,
-  channelsCreateV1, messageSendV1, messageReactV1, channelJoinV1, messageSendDmV1,
-  dmMessagesV1, userProfileUploadPhotoV1
+  clearV1, authRegisterV1, userProfileUploadPhotoV1
 } from './wrapperFunctions';
 
 beforeEach(() => {
   clearV1();
 });
 const MOSS_PHOTO = 'http://cdn.comedy.co.uk/images/library/people/180x200/t/the_it_crowd_moss.jpg'
+const PNG_PHOTO = 'https://www.transparentpng.com/download/water/xtbsSV-water-hd-photo.png'
 
 describe('Testing user/profile/uploadphoto/v1 success handling', () => {
   test('correct return type after uploading photo', () => {
     const user1 = authRegisterV1('hangpham@gmail.com', 'password', 'Hang', 'Pham');
-    const result = userProfileUploadPhotoV1(user1.token, MOSS_PHOTO, 0, 0, 5, 5);
+    const result = userProfileUploadPhotoV1(user1.token, MOSS_PHOTO, 0, 0, 50, 50);
     expect(result).toMatchObject({});
   });
+  
+});
+describe('Testing user/profile/uploadphoto/v1 error case handling', () => {
 
+  test.each([
+    {
+      desc: 'Token is invalid', token: 'InvalidToken', imgUrl: MOSS_PHOTO,
+      xStart: 0, yStart: 0, xEnd: 100, yEnd: 100, statusCode: FORBIDDEN
+    },
+    {
+      desc: 'imgUrl invalid', token: '', imgUrl: 'http://totallynotreal.com.au/picture.jpg',
+      xStart: 0, yStart: 0, xEnd: 100, yEnd: 100, statusCode: BAD_REQUEST
+    },
+    {
+      desc: 'xStart coordinate out of bounds', token: '', imgUrl: MOSS_PHOTO,
+      xStart: 5000, yStart: 0, xEnd: 100, yEnd: 100, statusCode: BAD_REQUEST
+    },
+    {
+      desc: 'yStart coordinate out of bounds', token: '', imgUrl: MOSS_PHOTO,
+      xStart: 0, yStart: 1000, xEnd: 100, yEnd: 100, statusCode: BAD_REQUEST
+    },
+    {
+      desc: 'xEnd coordinate out of bounds', token: '', imgUrl: MOSS_PHOTO,
+      xStart: 0, yStart: 0, xEnd: 1000, yEnd: 100, statusCode: BAD_REQUEST
+    },
+    {
+      desc: 'yEnd coordinate out of bounds', token: '', imgUrl: MOSS_PHOTO,
+      xStart: 0, yStart: 0, xEnd: 100, yEnd: 1000, statusCode: BAD_REQUEST
+    },
+    {
+      desc: 'xEnd is less than or equal to xStart', token: '', imgUrl: MOSS_PHOTO,
+      xStart: 20, yStart: 0, xEnd: 20, yEnd: 100, statusCode: BAD_REQUEST
+    },
+    {
+      desc: 'yEnd is less than or equal to yStart', token: '', imgUrl: MOSS_PHOTO,
+      xStart: 0, yStart: 20, xEnd: 100, yEnd: 20, statusCode: BAD_REQUEST
+    },
+    {
+      desc: 'image is not a .jpg', token: '', imgUrl: PNG_PHOTO,
+      xStart: 0, yStart: 20, xEnd: 100, yEnd: 20, statusCode: BAD_REQUEST
+    },
+
+  ])('$desc', ({ token, imgUrl, xStart, yStart, xEnd, yEnd, statusCode}) => {
+    const user1 = authRegisterV1('hangpham@gmail.com', 'password', 'Hang', 'Pham');
+    const result = userProfileUploadPhotoV1(user1.token + token, imgUrl,
+    xStart, yStart, xEnd, yEnd);
+  
+    expect(result.statusCode).toBe(statusCode);
+    const bodyObj = JSON.parse(result.body as string);
+    expect(bodyObj.error).toStrictEqual({ message: expect.any(String) });
+  });
 });
