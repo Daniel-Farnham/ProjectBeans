9824c2b491c918d67d5d1a22abcdbca679aa3be5
Bubleen Dhiman <z5421288@ad.unsw.edu.au>
2022-11-17 23:22:20 +1100

    implementation for authPasswordResetV1

--- src/auth.ts
+++ src/auth.ts
@@ -323,7 +323,7 @@ export async function authPasswordResetRequestV1(email: string) {
   setData(data);
   return {};
 }
-
+ 
 /**
  * Generates a reset code which is stored in dataStore
  * @returns {resetCode}
@@ -341,15 +341,15 @@ function generateResetCode(): any {
 }
 
 /**
- * Given a reset code for a user, sets that user's new password to the password provided.
+ * Given a reset code for a user, sets that user's new password to the password provided. 
  * Once a reset code has been used, it is then invalidated.
- *
- * @param {number} resetCode
- * @param {string} newPassword
- * @returns
+ * 
+ * @param {number} resetCode 
+ * @param {string} newPassword 
+ * @returns 
  */
 export function authPasswordResetV1(resetCode: string, newPassword: string) {
-  const data = getData();
+  const data = getData(); 
 
   const reset = data.resetCodeRequests.find((resetCodeRequests) => resetCodeRequests.resetCode === resetCode);
   if (typeof reset === 'undefined') {
@@ -359,5 +359,6 @@ export function authPasswordResetV1(resetCode: string, newPassword: string) {
     throw HTTPError(400, 'newPassword is too short.');
   }
   setData(data);
-  return {};
+
+  return {}
 }
