90d2a949231485fa58806c5c0a9f48c3bdfa0777
cs1531 <cs1531@cse.unsw.edu.au>
2022-11-27 03:24:15 +1100

    Results push

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-WORKING_DIRECTORY="~/www/cs1531deploy"
-
-USERNAME="w17cboost"
-SSH_HOST="ssh-w17cboost.alwaysdata.net"
-
-scp -r ./package.json ./tsconfig.json ./src "$USERNAME@$SSH_HOST:$WORKING_DIRECTORY"
-ssh "$USERNAME@$SSH_HOST" "cd $WORKING_DIRECTORY && npm install --omit=dev"
